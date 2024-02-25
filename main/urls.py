9e532f94b3c5c801b6e3b403450fc008ec289d44
Nikita Bushuev <nikita-1707@yandex.ru>
2024-02-25 12:48:34 +0300

    Add DB for my serverApp

--- main/urls.py
+++ main/urls.py
@@ -18,8 +18,8 @@ Including another URLconf
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.index),
-    path('about/', views.about),
-    path('partners/', views.partners),
-    path('profile/', views.profile),
+    path('',views.index, name = 'home'),
+    path('about/', views.about, name = 'about'),
+    path('partners/', views.partners, name = 'partners'),
+    path('profile/', views.profile, name = 'profile'),
 ]
